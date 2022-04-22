7c499621800e9198fdac60bd12853f815ecaed6b
Priyanshu Singh <priyanshusingh0082@gmail.com>
2022-04-22 22:34:55 +0530

    Updated APIs

--- src/app.js
+++ src/app.js
@@ -1,22 +1,46 @@
+// Module imports
+const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+
+// Local imports
 const movie_suggestions = require('./utils/movie_suggestions')
 const movie_details = require('./utils/movie_details')
 const list_movies = require('./utils/list_movies')
 const images = require('./utils/images')
-const { contentType } = require('express/lib/response')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views path
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+// Request handlers
 app.get('', (req, res)=>{
-    res.send({
-        home: 'Home',
-        name: 'CRYP73R'
+    res.render('index', {
+        title: 'Home',
+        author: 'CRYP73R'
     })
 })
 
-app.get('/list_movies', (req, res)=>{
+app.get('/documentation', (req, res)=>{
+    res.send({
+        status: 'success',
+        status_message: 'Query was Successful'
+    })
+})
+
+app.get('/api/list_movies', (req, res)=>{
     const limit=req.query.limit || 20
     const page=req.query.page || 1
     const quality=req.query.quality || 'all'
@@ -37,7 +61,7 @@ app.get('/list_movies', (req, res)=>{
     })
 })
 
-app.get('/movie_details', (req, res)=>{
+app.get('/api/movie_details', (req, res)=>{
     const movie_id = req.query.movie_id
     const with_images = req.query.with_images || false
     const with_cast = req.query.with_cast || false
@@ -58,7 +82,7 @@ app.get('/movie_details', (req, res)=>{
     })
 })
 
-app.get('/movie_suggestions', (req, res)=>{
+app.get('/api/movie_suggestions', (req, res)=>{
     const movie_id = req.query.movie_id
     if (!movie_id) {
         return res.send({
@@ -78,12 +102,12 @@ app.get('/movie_suggestions', (req, res)=>{
     })
 })
 
-app.get('/images', (req, res)=>{
-    const url = req.query.url
+app.get('/api/images', (req, res)=>{
+    const url = req.query.url.replace(/\\/g, '')
     if (!url) {
         return res.send({
             status: 'error',
-            status_message: 'URL required!'
+            status_message: 'Image URL required!'
         })
     }
     images.images(url, (error, imageData)=>{
@@ -95,6 +119,20 @@ app.get('/images', (req, res)=>{
         }
         res.set('Content-Type', 'image/jpeg')
         res.send(imageData)
+    })
+})
+
+app.get('/api/*', (req, res)=>{
+    res.render('404', {
+        title: '404 API not found',
+        author: 'CRYP73R'
+    })
+})
+
+app.get('/*', (req, res)=>{
+    res.render('404', {
+        title: '404 Page not found',
+        author: 'CRYP73R'
     })
 })
 
